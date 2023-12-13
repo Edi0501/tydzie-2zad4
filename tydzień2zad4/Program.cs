e16a1a6b5ba10da03ab6e961bb2382972bcfd5af
Edi0501 <edytanowakowska05@gmail.com>
2023-12-13 12:01:54 +0100

    Add project files.

---
+++ tydzień2zad4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tydzień2zad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stwórz zmienne i przypisz im odpowiednie wartości");
+
+            int value = 10;
+            string napis = "Szkoła Dotneta";
+            double value2 = 12.5;
+
+            Console.WriteLine(value + napis + value2);
+
+            Console.ReadKey();
+        }
+    }
+}
